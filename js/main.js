49f03bdf1fed958d70943d52a518caa1c1308176
wiztemple <wiztemple@gmail.com>
2019-05-06 11:34:33 +0100

    feat(cart): implement cart
    - add a dropdown for cart display
    - add a toggle function display and hide cart
    
    [Finishes]

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const searchIcon = document.getElementById('search-icon');
 const searchField = document.querySelector('.search-field');
 const closeIcon = document.querySelector('.close-icon');
+const bagIcon = document.querySelector('.clickBag');
+const cart = document.querySelector('.cart');
+const closeCart = document.querySelector('.closeCart');
 
 function displaySearchField() {
   searchField.style.display = 'block';
@@ -10,9 +13,20 @@ function closeSearchField() {
   searchField.style.display = 'none';
 }
 
+function displayCart() {
+  cart.style.display = 'block';
+}
+
+// function closeCart() {
+//   cart.style.display = 'none';
+// }
+
 function eventListener() {
   searchIcon.addEventListener('click', displaySearchField);
   closeIcon.addEventListener('click', closeSearchField);
+  bagIcon.addEventListener('click', displayCart);
+  // closeCart.addEventListener('click', closeCart);
 }
 
 eventListener();
+
