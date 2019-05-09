93d5e2976ff35f6e3de019b2fe31da87e4dd45a9
wiztemple <wiztemple@gmail.com>
2019-05-09 12:30:01 +0100

    bug(bug-fix): fix visible bugs
    - add sign up and sign in modals
    - create functions to handle reusable blocks of code
    - add style and animation for modals
    
    [Fixes]

---
+++ js/cart.js
@@ -0,0 +1,16 @@
+
+
+const elements = [];
+
+elements.forEach.call(document.querySelectorAll('a'), function (el) {
+  el.addEventListener('click', function () {
+    // code…
+  })
+})
+
+$("button").on("click", function () {
+  $(this).closest("li").find("img").clone().addClass("zoom").appendTo("body");
+  setTimeout(function () {
+    $(".zoom").remove()
+  }, 1000)
+});
--- js/main.js
+++ js/main.js
@@ -1,45 +1,117 @@
+const authLinks = document.querySelectorAll('.authLink');
+const signinLink = document.querySelector('.signinLink');
+const signin = document.querySelector('.signin');
+const signup = document.querySelector('.signup');
 const searchIcon = document.getElementById('search-icon');
 const searchField = document.querySelector('.search-field');
-const closeIcon = document.querySelector('.close-icon');
-const bagIcon = document.querySelector('.clickBag');
+const closeIcons = document.querySelectorAll('.closeIcon');
 const cart = document.querySelector('.cart');
-const closeCart = document.querySelector('.closeCart');
+const cartIcons = document.querySelectorAll('.cartIcon');
 
-function displaySearchField() {
-  searchField.style.display = 'block';
+$("button").on("click", function() {
+  $(this).closest(".card-overlay").find("img").clone().addClass("zoom").appendTo("body");
+  setTimeout(function () {
+    $(".zoom").remove()
+  }, 1000)
+});
+/**
+ * Display the signup form
+ *
+ * @param {null}
+ *
+ * @returns signup modal
+ */
+const displaySignup = () => signup.style.display = 'block';
+ 
+/**
+ * Display the signup form
+ *
+ * @param {null}
+ *
+ * @returns signin modal
+ */
+const displaySignin = () => signin.style.display = 'block';
+
+/**
+ * Display the search field on the navbar
+ *
+ * @param {null}
+ *
+ * @returns search field
+ */
+const displaySearchField = () => searchField.style.display = 'block';
+
+/**
+ * Display the cart items table
+ *
+ * @param {null}
+ *
+ * @returns cart items
+ */
+const displayCart = () => cart.style.display = 'block';
+
+/**
+ * Makes the closeIcon re-usable
+ *
+ * @param {null}
+ *
+ * @returns
+ */
+[...closeIcons].map((closeIcon) => {
+  closeIcon.addEventListener('click', () => {
+    const div = closeIcon.closest('.target-js');
+    div.style.display = 'none';
+  });
+});
+
+/**
+ * makes the function to display a cart items re-usable
+ *
+ * @param {null}
+ *
+ * @returns cart items
+ */
+[...cartIcons].map(cartIcon => {
+  cartIcon.addEventListener('click', displayCart);
+});
+
+[...authLinks].map(authLink => {
+  authLink.addEventListener('click', displaySignup);
+})
+
+if (signinLink) {
+  signinLink.addEventListener('click', displaySignin);
 }
 
-function closeSearchField() {
-  searchField.style.display = 'none';
-}
+searchIcon.addEventListener('click', displaySearchField);
 
-function displayCart() {
-  cart.style.display = 'block';
-}
+/**
+ * Displays the current image on the details page
+ *
+ * @param {String} image
+ *
+ * @returns
+ */
+const container = document.getElementById('image-container');
 
-// function closeCart() {
-//   cart.style.display = 'none';
-// }
+const changeImage = image => container.src = image.src;
 
-function eventListener() {
-  searchIcon.addEventListener('click', displaySearchField);
-  closeIcon.addEventListener('click', closeSearchField);
-  bagIcon.addEventListener('click', displayCart);
-  // closeCart.addEventListener('click', closeCart);
-}
+/**
+ * Displays the menu-items on mobile view
+ *
+ * @param {null}
+ *
+ * @returns menu items
+ */
+ document.addEventListener('DOMContentLoaded', () => {
 
-eventListener();
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
   // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+    if ($navbarBurgers.length > 0) {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
+  // Add a click event on each of them
+     $navbarBurgers.forEach(el => {
       el.addEventListener('click', () => {
 
         // Get the target from the "data-target" attribute
@@ -48,10 +120,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
+         $target.classList.toggle('is-active');
+       });
+     });
   }
-
-});
+ });
